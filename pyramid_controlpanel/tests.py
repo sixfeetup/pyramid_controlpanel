42d1180f7e1ec57bbaca0f9ae28fd2482edc5167
David Blewett <davidb@sixfeetup.com>
2013-05-10 02:24:48 -0400

    Add test to ensure that a schema field's default value bubbles up correctly.

--- pyramid_controlpanel/tests.py
+++ pyramid_controlpanel/tests.py
@@ -1,17 +1,43 @@
-import unittest
+from unittest import TestCase
 
 from pyramid import testing
 
+from pyramid_controlpanel.views import ControlPanel
 
-class ViewTests(unittest.TestCase):
+from colander import (Integer, Schema, SchemaNode)
+
+from sixfeetup.bowab.tests.mocks import MockSession
+
+
+TOKEN_DURATION = 60
+class Authentication(Schema):
+    token_duration = SchemaNode(
+        Integer(),
+        default=TOKEN_DURATION,
+        description=u'Duration (in minutes) password reset tokens are valid for.'
+    )
+authentication_schema = Authentication(
+    path=u'.'.join((Authentication.__module__,
+                   'authentication_schema')),
+    name=u'authentication_schema',
+    title=u'Authentication',
+)
+
+class ViewTests(TestCase):
     def setUp(self):
+        super(ViewTests, self).setUp()
         self.config = testing.setUp()
+        self.config.include('pyramid_controlpanel')
+        self.config.add_controlpanel_section(authentication_schema)
 
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
+    def test_get_value(self):
+        # Make sure the default value bubbles up correctly
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'pyramid_controlpanel')
+        request.user = None
+        request.db_session = MockSession()
+        cp = ControlPanel(request)
+        authentication_schema
+        token_duration = cp.get_value(authentication_schema.name,
+                                      'token_duration')
+        self.assertTrue(token_duration == TOKEN_DURATION)
+
